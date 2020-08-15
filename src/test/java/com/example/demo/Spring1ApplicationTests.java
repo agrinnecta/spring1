ac5404e8952017ccdb64b1deaa8de3351ab54af8
lincner <lincner@gmail.com>
2020-08-15 13:12:12 +0200

    Names for the REST API test as a constant

--- src/test/java/com/example/demo/Spring1ApplicationTests.java
+++ src/test/java/com/example/demo/Spring1ApplicationTests.java
@@ -12,13 +12,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Spring1ApplicationTests {
 
+	private static final String[] NAMES = new String[] {"Ana","Peter"}; 
+	
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
 	void getNamesShouldReturnExpectedNames() {
-		Set<String> resultNames = new HashSet<String>(Arrays.asList("Ana","Peter"));
+		Set<String> resultNames = new HashSet<String>(Arrays.asList(NAMES));
 		RestDemoController restDemoController = new RestDemoController();
 		assertArrayEquals(resultNames.toArray(), restDemoController.getNames().toArray());
 	}
